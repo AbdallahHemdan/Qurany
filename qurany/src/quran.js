1e6aef19740bc3aff190b2d3c1690b7c3e7b09f2
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-06-17 15:26:18 +0200

    Add more quran tracks which I like to listen

--- qurany/src/quran.js
+++ qurany/src/quran.js
@@ -24,6 +24,36 @@ const listOfQuran = [
     artist: "القطامي",
     src: require("./assets/quran/alkatamy2.mp3"),
   },
+  {
+    title: "سورة البروج",
+    artist: "رعد الكردي",
+    src: require("./assets/quran/ra3d.mp3"),
+  },
+  {
+    title: "سورة الإنشقاق",
+    artist: "الحصري",
+    src: require("./assets/quran/hosary.mp3"),
+  },
+  {
+    title: "سورة الحديد",
+    artist: "عبد الرشيد صوفي",
+    src: require("./assets/quran/sofy.mp3"),
+  },
+  {
+    title: "الحاقة",
+    artist: "عبدالرحمن العوسي",
+    src: require("./assets/quran/osy.mp3"),
+  },
+  {
+    title: "فاطر",
+    artist: "الشيخ نديم",
+    src: require("./assets/quran/nadem.mp3"),
+  },
+  {
+    title: "وصف الجنة",
+    artist: "منصور السالمي",
+    src: require("./assets/quran/mansour.mp3"),
+  },
 ];
 
 export default listOfQuran;
