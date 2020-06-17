fbb4621caaed6c063961d1e661a9d0cc80a4a111
AbdallahHemdan <abdallah.ahmed.hemdan@gmail.com>
2020-06-17 13:05:09 +0200

    Add list of quran media sound

---
+++ qurany/src/quran.js
@@ -0,0 +1,29 @@
+const listOfQuran = [
+  {
+    title: "سورة الحجر",
+    artist: "هزاع البلوشي",
+    src: require("./assets/quran/haza3.mp3"),
+  },
+  {
+    title: "لا إله الا هو سبحانه",
+    artist: "القطامي",
+    src: require("./assets/quran/alkatamy.mp3"),
+  },
+  {
+    title: "روائع التلاوة",
+    artist: "الحصري",
+    src: require("./assets/quran/elhosary.mp3"),
+  },
+  {
+    title: "ألم تر إلي الذين قيل كفواأيديكم",
+    artist: "القطامي",
+    src: require("./assets/quran/videoplayback.mp3"),
+  },
+  {
+    title: "فلا تلوموني ولوموا أنفسكم",
+    artist: "القطامي",
+    src: require("./assets/quran/alkatamy2.mp3"),
+  },
+];
+
+export default listOfQuran;
